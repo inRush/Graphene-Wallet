59597d996df26e65f3643feca9b0026a48493ef7
inRush <in_rush@outlook.com>
2018-04-13 09:23:26 +0800

    修改备份页面的密码弹框

--- app/src/main/java/com/gxb/gxswallet/page/backuptip/BackUpTipActivity.java
+++ app/src/main/java/com/gxb/gxswallet/page/backuptip/BackUpTipActivity.java
@@ -3,13 +3,14 @@ package com.gxb.gxswallet.page.backuptip;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.TextView;
 
 import com.gxb.gxswallet.R;
+import com.gxb.gxswallet.base.dialog.PasswordDialog;
 import com.gxb.gxswallet.db.wallet.WalletData;
 import com.gxb.gxswallet.db.wallet.WalletDataManager;
+import com.gxb.gxswallet.page.main.MainActivity;
 import com.gxb.gxswallet.services.WalletService;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
@@ -23,7 +24,7 @@ import butterknife.OnClick;
  * @date 2018/4/1.
  */
 
-public class BackUpTipActivity extends BaseActivity {
+public class BackUpTipActivity extends BaseActivity implements PasswordDialog.OnPasswordConfirmListener {
 
     @BindView(R.id.topbar_backup_tip)
     QMUITopBar mTopBar;
@@ -35,13 +36,16 @@ public class BackUpTipActivity extends BaseActivity {
 
     private static final int EXPORT_LOADING_KEY = 0x100;
     private static final String DATA_KEY = "wallet";
+    private static final String START_KEY = "main";
     private WalletData mWalletData;
+    private boolean isStartMain;
     private WalletDataManager mWalletDataManager;
 
 
-    public static void start(Activity activity, WalletData wallet) {
+    public static void start(Activity activity, WalletData wallet, boolean startMain) {
         Intent intent = new Intent(activity, BackUpTipActivity.class);
         intent.putExtra(DATA_KEY, wallet);
+        intent.putExtra(START_KEY, startMain);
         activity.startActivity(intent);
     }
 
@@ -49,6 +53,7 @@ public class BackUpTipActivity extends BaseActivity {
     protected boolean initArgs(Bundle bundle) {
         if (bundle != null) {
             mWalletData = bundle.getParcelable(DATA_KEY);
+            isStartMain = bundle.getBoolean(START_KEY);
             return super.initArgs(bundle);
         }
         return false;
@@ -58,12 +63,7 @@ public class BackUpTipActivity extends BaseActivity {
     protected void initWidget() {
         super.initWidget();
         mTopBar.setTitle(getString(R.string.backup_wallet));
-        mTopBar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showConfirm();
-            }
-        });
+        mTopBar.addLeftBackImageButton().setOnClickListener(v -> showConfirm());
     }
 
     @Override
@@ -83,10 +83,16 @@ public class BackUpTipActivity extends BaseActivity {
                     mWalletData.setIsBackUp(true);
                     mWalletDataManager.update(mWalletData);
                     finish();
+                    if (isStartMain) {
+                        MainActivity.start(this);
+                    }
                 })
                 .addAction("下次再说", (dialog, index) -> {
                     dialog.dismiss();
                     finish();
+                    if (isStartMain) {
+                        MainActivity.start(this);
+                    }
                 })
                 .show();
     }
@@ -98,27 +104,22 @@ public class BackUpTipActivity extends BaseActivity {
 
     @OnClick(R.id.backup_wallet_btn_backup_tip)
     void onBackupWalletBtnClick() {
-        final QMUIDialog.EditTextDialogBuilder builder = new QMUIDialog.EditTextDialogBuilder(this);
-        builder.setTitle(getString(R.string.input_password))
-                .setPlaceholder(getString(R.string.input_password_here))
-                .setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD)
-                .addAction(getString(R.string.cancel), (dialog, index) -> dialog.dismiss())
-                .addAction(getString(R.string.ok), (dialog, index) -> {
-                    String password = builder.getEditText().getText().toString();
-                    String[] keys = WalletService.getInstance()
-                            .unlockWallet(mWalletData, password);
-                    if (keys[0] == null) {
-                        showError(getString(R.string.password_error));
-                    } else {
-                        mUnlockWalletHintTV.setVisibility(View.GONE);
-                        if (keys[1] != null) {
-                            mMemorizingWordTv.setText(keys[1]);
-                        } else {
-                            showError(getString(R.string.wallet_already_backup));
-                        }
-                    }
-                    dialog.dismiss();
-                })
-                .show();
+        new PasswordDialog().setPasswordConfirmListener(this).show(getSupportFragmentManager(), "password");
+    }
+
+    @Override
+    public void onConfirm(String password) {
+        String[] keys = WalletService.getInstance()
+                .unlockWallet(mWalletData, password);
+        if (keys[0] == null) {
+            showError(getString(R.string.password_error));
+        } else {
+            mUnlockWalletHintTV.setVisibility(View.GONE);
+            if (keys[1] != null) {
+                mMemorizingWordTv.setText(keys[1]);
+            } else {
+                showError(getString(R.string.wallet_already_backup));
+            }
+        }
     }
 }
