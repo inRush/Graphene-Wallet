1a5d6a691e2dc3f1aeccabe4aa1386f542ae1249
inRush <in_rush@outlook.com>
2018-05-06 17:10:09 +0800

    删除不必要的类以及移动一些类到正确的包中

--- app/src/main/java/com/gxb/gxswallet/base/keep.java
+++
@@ -1,9 +0,0 @@
-package com.gxb.gxswallet.base;
-
-/**
- * @author inrush
- * @date 2018/3/26.
- */
-
-public class keep {
-}
---
+++ app/src/main/java/com/gxb/gxswallet/manager/AssetManager.java
@@ -0,0 +1,9 @@
+package com.gxb.gxswallet.manager;
+
+/**
+ * @author inrush
+ * @date 2018/5/6.
+ */
+
+public class AssetManager {
+}
